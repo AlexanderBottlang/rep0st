215e0a25c55878cdc31cdd77f5b5c4c1504fe2bb
ReneHollander <rene.hollander@hotmail.de>
2015-06-03 15:44:46 +0200

    fixed postgresql config to allow connections from host machine

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,10 @@ apt-get update
 apt-get dist-upgrade -y
 apt-get install -y gcc g++ make cmake binutils build-essential unzip checkinstall htop  git postgresql postgresql-server-dev-all
 
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '0.0.0.0'/g" /etc/postgresql/9.3/main/postgresql.conf
+sed -i '75ihost       all          all             192.168.1.0/24          trust' /etc/postgresql/9.3/main/pg_hba.conf
+service postgresql restart
+
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 git clone https://github.com/eulerto/pg_similarity.git
